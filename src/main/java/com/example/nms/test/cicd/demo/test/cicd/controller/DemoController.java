1e3a89e76fb7c4c5bf06e86eec11571eedce4078
duymai <rucro12345@gmai.com>
2021-06-18 09:47:28 +0700

    first commit

---
+++ src/main/java/com/example/nms/test/cicd/demo/test/cicd/controller/DemoController.java
@@ -0,0 +1,49 @@
+package com.example.nms.test.cicd.demo.test.cicd.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping("/api/v1")
+public class DemoController {
+
+    @GetMapping("/demoData")
+    public User getAllEmployees() {
+
+        return new User("duy",29);
+    }
+}
+
+class User {
+    String name;
+    Integer age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public User() {
+    }
+
+    public User(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+}
